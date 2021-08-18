7695529db4594fe1ed9201b97571d071b9d2b5a5
jeffersonzup <jefferson.wiltenburg@zup.com.br>
2021-08-18 10:26:57 -0300

    Setup do projeto.

---
+++ src/test/java/br/com/zupacademy/jefferson/microservicepropostas/MicroservicePropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.jefferson.microservicepropostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
